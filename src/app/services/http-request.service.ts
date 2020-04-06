460f967f85bc9d1c634b2522201826105b6ce140
hamisicodes <newhamisi@gmail.com>
2020-04-06 14:59:41 +0300

    Implement the http-request service using a promise

--- src/app/services/http-request.service.ts
+++ src/app/services/http-request.service.ts
@@ -10,4 +10,39 @@ export class HttpRequestService {
 
 
   constructor(private http:HttpClient) { }
+
+  userRequest(){
+    interface ApiResponse{
+      login:string;
+      avatar_url:string;
+      followers:number;
+      following:number;
+      public_repos:number
+    }
+    let newPromise  = new Promise((resolve,reject)=>{
+      this.http.get<ApiResponse>("https://api.github.com/users/hamisicodes").toPromise().then(response=>{
+        this.user.login = response.login
+        this.user.avatar_url = response.avatar_url
+        this.user.followers = response.followers
+        this.user.following = response.following
+        this.user.public_repos = response.public_repos
+
+        resolve()
+
+      },error=>{
+        this.user.login = "none"
+        this.user.avatar_url = "none"
+        this.user.followers = 0
+        this.user.following = 0
+        this.user.public_repos = 0
+        
+        reject(error)
+
+      })
+
+    })
+    return newPromise
+
+  }
+  
 }
