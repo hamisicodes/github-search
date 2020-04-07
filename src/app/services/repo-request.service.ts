00d390fa1de2c1d352c9584667595c52b85f376d
hamisicodes <newhamisi@gmail.com>
2020-04-07 11:13:18 +0300

    return an observable of typerepository in repoRequest function

--- src/app/services/repo-request.service.ts
+++ src/app/services/repo-request.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
+import { Repository } from '../repository';
 
 
 @Injectable({
@@ -9,4 +10,7 @@ import {HttpClient} from '@angular/common/http';
 export class RepoRequestService {
 
   constructor(private http:HttpClient) { }
+  repoRequest():Observable<Repository>{
+    return this.http.get<Repository>("https://api.github.com/users/hamisicodes/repos");
+  }
 }
