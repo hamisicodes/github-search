b59f3b05c534c5a6e1c85f59b3ee33e840a95a3e
hamisicodes <newhamisi@gmail.com>
2020-04-07 22:30:57 +0300

    define the path to be taken by an unknwn route to follow the NotFound component

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from '../profile/profile.component';
 import { ReposListComponent } from '../repos-list/repos-list.component';
+import { NotFoundComponent } from '../not-found/not-found.component';
 
 
 
@@ -9,6 +10,7 @@ const routes: Routes = [
   {path:'profile',component:ProfileComponent},
   {path:'repos',component:ReposListComponent},
   { path: '', redirectTo:"profile", pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
 ];
 
 @NgModule({
