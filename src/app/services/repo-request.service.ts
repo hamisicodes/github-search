11ee55217ad9558e0adc3ba37ca3e0dd701e7e25
hamisicodes <newhamisi@gmail.com>
2020-04-08 10:04:38 +0300

    add APIkey to our reporequest

--- src/app/services/repo-request.service.ts
+++ src/app/services/repo-request.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import { Repository } from '../repository';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -12,6 +13,6 @@ export class RepoRequestService {
   constructor(private http:HttpClient) { }
 
   repoRequest(username):Observable<Repository[]>{
-    return this.http.get<Repository[]>("https://api.github.com/users/" + username + "/repos");
+    return this.http.get<Repository[]>("https://api.github.com/users/" + username + "/repos" + environment.apiKey);
   }
 }
