4af20c5d899c61a2b00d5de02c4d76b9fafe5e70
hamisicodes <newhamisi@gmail.com>
2020-04-10 22:41:14 +0300

    Add a button todisplay repositries

--- src/app/services/repo-request.service.ts
+++ src/app/services/repo-request.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import { Repository } from '../repository';
-import { environment } from 'src/environments/environment';
+import {environment } from '../../environments/environment';
 
 
 @Injectable({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey : '?access_token='+ 'c1ac6691ab68063c57eb8ec732cccf929088fe8c'
 };
