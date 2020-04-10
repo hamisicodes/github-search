0debff05d1ea699a8a7163f985e1ddf06b6bc88f
hamisicodes <newhamisi@gmail.com>
2020-04-10 23:07:06 +0300

    Inject  reporequest service and use it in profile component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { HttpRequestService } from '../services/http-request.service';
+import { RepoRequestService } from '../services/repo-request.service';
+import { Repository } from '../repository';
 
 @Component({
   selector: 'app-profile',
@@ -12,18 +14,27 @@ import { HttpRequestService } from '../services/http-request.service';
 export class ProfileComponent implements OnInit {
   
   user:User;
+  repo:Repository[]
 
   searchUserName(username:string){
   
     this._httpRequest.userRequest(username);
     this.user = this._httpRequest.user;
+    this._repo.repoRequest(username).subscribe(data=>{
+      this.repo = data
+      
+    })
 
   }
 
-  constructor(private _httpRequest:HttpRequestService) { }
+  constructor(private _httpRequest:HttpRequestService,private _repo:RepoRequestService) {
+ 
+   }
+ 
 
   ngOnInit(): void {
     this.searchUserName('hamisicodes')
+    
   
   }
 
