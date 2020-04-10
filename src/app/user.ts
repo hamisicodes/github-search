e5fdf15d77f2c5951c58e8920dc16fcb38b2e1a5
hamisicodes <newhamisi@gmail.com>
2020-04-10 14:59:00 +0300

    final changes

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,5 +1,5 @@
 export class User {
-    constructor(public login:string,public avatar_url:string,public followers:number,public following:number,public public_repos:number){
+    constructor(public login:string , public avatar_url:string ,  public followers:number , public following:number, public public_repos:number){
 
     }
 
